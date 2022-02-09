c9a81c7e13908ab4fd7e2877a90b10d01c26fa93
Harkirat Singh <honey.harkirat@outlook.com>
2022-02-09 15:02:42 +0530

    Stars in background

---
+++ star.py
@@ -0,0 +1,18 @@
+import pygame
+from res import *
+
+class star(pygame.sprite.Sprite):
+    def __init__(self, origin):
+        super().__init__()
+        self.x = origin[0]
+        self.y = origin[1]
+        self.valid = True
+    
+    def update(self):
+        if self.y < SCREEN_HEIGHT:
+            self.y += STAR_DISP
+        else:
+            self.valid = False
+
+    def draw(self, surface):
+        pygame.draw.circle(surface, (255,255,255), (self.x, self.y), 1)
