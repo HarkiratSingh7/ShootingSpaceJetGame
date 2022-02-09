dca2d548688ffdde8f60d25788266d267a0c4aed
Harkirat Singh <honey.harkirat@outlook.com>
2022-02-09 07:48:51 +0530

    Resources defined

---
+++ res.py
@@ -0,0 +1,30 @@
+# Resources and constants
+
+# Screen Constraints
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+FPS = 60
+
+# Strings
+TITLE = 'Shooting Jet Game'
+
+# Config
+JET_CENTER = (SCREEN_WIDTH // 2, int(0.85 * SCREEN_HEIGHT))
+JET_SCALE = (80, 80)
+SHOT_SCALE = (20, 20)
+ENEMY_SCALE = (80, 80)
+
+# Colors
+DARK = (20, 20, 20)
+LIGHT = (200, 200, 200)
+
+# Images
+JET_IMG = 'jet.png'
+SHOT_IMG = 'shot.png'
+ENEMY_IMG = 'enemy.png'
+
+# Displacement Constraints
+DX_JET = 5
+DY_JET = 5
+DY_SHOT = 3
+DY_ENEMY = 2
