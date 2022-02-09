744f1ee5e49c4d96679b42f54f7b040c5126e657
Harkirat Singh <honey.harkirat@outlook.com>
2022-02-09 15:01:45 +0530

    Resources organised

--- res.py
+++ res.py
@@ -6,22 +6,26 @@ SCREEN_HEIGHT = 600
 FPS = 60
 
 # Strings
-TITLE = 'Shooting Jet Game'
+TITLE = 'Shooting Space Jet Game'
 
 # Config
 JET_CENTER = (SCREEN_WIDTH // 2, int(0.85 * SCREEN_HEIGHT))
 JET_SCALE = (80, 80)
 SHOT_SCALE = (20, 20)
 ENEMY_SCALE = (80, 80)
+EXPLOSION_SCALE = (50, 50)
+FONT_SIZE = 32
+SCORE_PADDING = 10
 
 # Colors
 DARK = (20, 20, 20)
 LIGHT = (200, 200, 200)
 
 # Images
-JET_IMG = 'jet.png'
-SHOT_IMG = 'shot.png'
-ENEMY_IMG = 'enemy.png'
+JET_IMG = '.\\assets\\jet.png'
+SHOT_IMG = '.\\assets\\shot.png'
+ENEMY_IMG = '.\\assets\\enemy.png'
+EXPLOSION_IMG = '.\\assets\\explosion.png'
 
 # Constraints
 DX_JET = 5
@@ -32,3 +36,7 @@ ALLOWED_JET_ENEMY_OVERLAP = 30
 MIN_GEN_ENEMIES = 0
 MIN_ENSURE_ENEMIES = 2
 MAX_GEN_ENEMIES = 5
+POINTS_FOR_HITTING_ENEMY = 5
+STARS_N = 100
+STAR_DISP = 3
+EXPLOSION_LAST = 2
