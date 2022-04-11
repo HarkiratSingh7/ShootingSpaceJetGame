a026de2080d57f5dd22fee34dc705bf6dd8a20a7
Harkirat Singh <honey.harkirat@outlook.com>
2022-04-11 07:59:52 +0530

    Setup configurations added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='ShootingSpaceJetGame',
+    version='1.0',
+    packages=['shootingspacejetgame',],
+    license='MIT',
+    author='Harkirat Singh (honey.harkirat@outlook.com)',
+    long_description=open('Readme.md').read(),
+)
